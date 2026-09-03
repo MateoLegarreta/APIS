d2e7d6679987f746c249c39fe245e244d21bae6b
tomi0500 <tomisoisa@gmail.com>
2026-09-03 14:40:33 -0300

    Add files via upload

---
+++ demo/src/main/java/com/uade/tpo/demo/exceptions/CartItemNotFoundException.java
@@ -0,0 +1,9 @@
+package com.uade.tpo.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "El producto indicado no se encuentra en el carrito")
+public class CartItemNotFoundException extends Exception {
+
+}
